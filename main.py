e4ac5bc2280da179025fd63ebfe5f96712290315
Hunter Price <hunterprice4044@gmail.com>
2020-08-30 20:32:39 -0400

    Added workig model

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import digits_predictor as digits_predictor
+from sklearn import datasets
+
+dp = digits_predictor.DigitsPredictor(datasets.load_digits())
+dp.display_data()
+
+# this model does not fit the data well at all
+# dp.kmeans_model(.25,40,show=True)
+
+# this model fits and predicts the data with 99.1 percent accuracy
+dp.svc_model(.25,40)
